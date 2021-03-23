352f734b105466e5d31286aa2004829aad91988d
6872Vishal <77484050+6872Vishal@users.noreply.github.com>
2021-03-23 23:06:32 +0530

    Update sensor-validate.cpp

--- sensor-validate.cpp
+++ sensor-validate.cpp
@@ -8,30 +8,10 @@ bool difference_of_2nd_and_1stValue_greaterthan_3rd(double value, double nextVal
   return false;
 }
 
-// bool validateSOCreadings(double* values, int numOfValues) {
-//   int lastButOneIndex = numOfValues - 1;
-//   for(int i = 0; i < lastButOneIndex; i++) {
-//     if(difference_of_2nd_and_1stValue_greaterthan_3rd(values[i], values[i + 1], 0.05)) {
-//       return false;
-//     }
-//   }
-//   return true;
-// }
-
-// bool validateCurrentreadings(double* values, int numOfValues) {
-//   int lastButOneIndex = numOfValues - 1;
-//   for(int i = 0; i < lastButOneIndex; i++) {
-//     if(difference_of_2nd_and_1stValue_greaterthan_3rd(values[i], values[i + 1], 0.1)) {
-//       return false;
-//     }
-//   }
-//   return true;
-// }
-
 bool validateCurrentreadings(double* values, int numOfValues,DeltaTypes Which_Delta_Type) {
   int lastButOneIndex = numOfValues - 1;
   for(int i = 0; i < lastButOneIndex; i++) {
-    if(difference_of_2nd_and_1stValue_greaterthan_3rd(values[i], values[i + 1], MaxDelta[Which_Delta_Type])) {
+    if(difference_of_2nd_and_1stValue_greaterthan_3rd(values[i], values[i + 1], DeltaValue[Which_Delta_Type])) {
       return false;
     }
   }
