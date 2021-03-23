d58afdd85253b343903872010f7387a7a0895c1f
6872Vishal <77484050+6872Vishal@users.noreply.github.com>
2021-03-23 22:53:18 +0530

    Update sensor-validate.h

--- sensor-validate.h
+++ sensor-validate.h
@@ -1,6 +1,7 @@
+enum DeltaTypes = {Normal, SOC, Total_Deltas};
+
+double MaxDelta[Total_Deltas] = {0.1 , 0.05};
 
 bool difference_of_2nd_and_1stValue_greaterthan_3rd(double value, double nextValue, double maxDelta);
 
-bool validateSOCreadings(double* values, int numOfValues);
-
-bool validateCurrentreadings(double* values, int numOfValues, double margin);
+bool validateCurrentreadings(double* values, int numOfValues, int Which_Delta_Type);
