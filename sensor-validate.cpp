1a84121a8c7c12940361d186bbaf77739f28b1d1
6872Vishal <77484050+6872Vishal@users.noreply.github.com>
2021-03-24 00:09:23 +0530

    Update sensor-validate.cpp

--- sensor-validate.cpp
+++ sensor-validate.cpp
@@ -9,13 +9,24 @@ bool difference_of_2nd_and_1stValue_greaterthan_3rd(double value, double nextVal
   return false;
 }
 
-bool validateCurrentreadings(double* values, int numOfValues,DeltaTypes Which_Delta_Type) {
-  int lastButOneIndex = numOfValues - 1;
-  for(int i = 0;numOfValues != 0,i < lastButOneIndex; i++) {
-    if(difference_of_2nd_and_1stValue_greaterthan_3rd(values[i], values[i + 1], DeltaValue[Which_Delta_Type])) {
-      return false;
+bool Delta_Deviation_Occured(double* values_provided, int LastIndex, double Delta_Used)
+{
+  for(int i = 0;numOfValues != 0,i < LastIndex; i++) {
+    if(difference_of_2nd_and_1stValue_greaterthan_3rd(values_provided[i], values_provided[i + 1], Delta_Used) ) {
+      return true;
     }
   }
+  return false;
+}
 
+bool validateCurrentreadings(double* values, int numOfValues,DeltaTypes Which_Delta_Type) {
+
+  if(numOfValues != 0)
+  {
+    int lastButOneIndex = numOfValues - 1;
+      if(Delta_Deviation_Occured(values[0], lastButOneIndex, DeltaValue[Which_Delta_Type])) {
+        return false;
+    }
+  }
   return true;
 }
