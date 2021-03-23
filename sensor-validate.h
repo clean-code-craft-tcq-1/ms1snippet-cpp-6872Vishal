cee1705fefb38e635043867462352904fe76c352
6872Vishal <77484050+6872Vishal@users.noreply.github.com>
2021-03-23 23:14:49 +0530

    Update sensor-validate.h

--- sensor-validate.h
+++ sensor-validate.h
@@ -1,6 +1,6 @@
 enum DeltaTypes {Normal, SOC, Total_Deltas};
 
-extern double DeltaValue[Total_Deltas] = {0.1 , 0.05};
+static double DeltaValue[Total_Deltas] = {0.1 , 0.05};
 
 bool difference_of_2nd_and_1stValue_greaterthan_3rd(double value, double nextValue, double maxDelta);
 
