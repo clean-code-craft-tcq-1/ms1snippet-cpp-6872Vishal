13a08abfc9561d56dbd6ad7e03cba9ccf9fc82dd
6872Vishal <77484050+6872Vishal@users.noreply.github.com>
2021-03-23 23:55:43 +0530

    Update sensor-validate.cpp

--- sensor-validate.cpp
+++ sensor-validate.cpp
@@ -1,6 +1,7 @@
 #include "sensor-validate.h"
-//difference_of_NextandCurrentValue_greaterthan_precision
-//difference_of_2nd_and_1stValue_greaterthan_3rd
+
+double DeltaValue[Total_Deltas] = {0.1 , 0.05};
+
 bool difference_of_2nd_and_1stValue_greaterthan_3rd(double value, double nextValue, double maxDelta) {
   if(nextValue - value > maxDelta) {
     return true;
